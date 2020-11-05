fb4e83224815f232e330bcb94150682f0e0f3213
kaiquepill <kaique.gomes@bandtec.com.br>
2020-11-05 16:30:30 -0300

    feat: criado a estrutura do projeto e a imagem do docker

---
+++ dev_forum/src/main/kotlin/com/dev_forum/DevForumApplication.kt
@@ -0,0 +1,12 @@
+package com.dev_forum
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevForumApplication
+
+
+fun main(args: Array<String>) {
+	runApplication<DevForumApplication>(*args)
+}
---
+++ dev_forum/src/test/kotlin/com/dev_forum/DevForumApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dev_forum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevForumApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
